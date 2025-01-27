42e475e3d8b9289211064c9c85bbd8db828180d9
Ian Smith <iansmithtrainlover@gmail.com>
2025-01-26 22:18:56 -0600

    Updated color sort

--- include/refactor/color_sort.hpp
+++ include/refactor/color_sort.hpp
@@ -1,14 +1,21 @@
-/*#pragma once
+#pragma once
 
 #include "pros/optical.hpp"
 #include "pros/colors.hpp"
 #include "pros/distance.hpp"
 
 class Color_Sort {
+    public:
+    Color_Sort(pros::Optical *optical, pros::Distance *distance);
+
     pros::Color get_color();
 
     bool object_detected();
 
+    pros::Color object_color();
+
     pros::Color last_color;
+    private:
+    pros::Optical *optical;
+    pros::Distance *distance;
 };
-*/
--- src/refactor/color_sort.cpp
+++ src/refactor/color_sort.cpp
@@ -5,8 +5,19 @@ Color_Sort::Color_Sort (pros::Optical *optical, pros::Distance *distance) {
     this->distance = distance;
 }
 
-Color_Sort::get_color () {
+pros::Color Color_Sort::get_color () {
     pros::Color color = optical->get_rgb().red > optical->get_rgb().blue? pros::Color::red : pros::Color::blue;
     Color_Sort::last_color = color;
     return color;
+}
+
+bool Color_Sort::object_detected () {
+    return distance->get_distance() < 4;
+}
+
+pros::Color Color_Sort::object_color () {
+    if (Color_Sort::object_detected()) {
+        return Color_Sort::get_color();
+    }
+    return pros::Color::gray; // a default for if there isn't an object
 }
