2fb2e9aa8088cd022d13a1f20ff0e19faa10d732
Ian Smith <iansmithtrainlover@gmail.com>
2025-01-27 12:13:16 -0600

    Expanded intake

--- include/refactor/intake.hpp
+++ include/refactor/intake.hpp
@@ -3,14 +3,17 @@
 #include "pros/rotation.hpp"
 #include "pros/distance.hpp"
 #include "pros/colors.hpp"
+#include "color_sort.hpp"
+#include <vector>
 
-class intake {
-    bool is_spinning();
-    pros::Rotation get_direction();
-    pros::Color get_color();
-    bool object_detected();
-    pros::Color object_color();
-    pros::Color last_color;
+class Intake {
+    bool is_scheduled_motion();
+    std::vector<double> hook_poses;
+    void move_hook_to_ready();
+    void pickup_and_hold();
+    void load_lb();
+    void score_ring();
+    void sort_ring();
     
     private:
     pros::Rotation *rotation;
@@ -18,4 +21,11 @@ class intake {
     pros::Distance *lower_distance;
     pros::Color *upper_color;
     pros::Color *lower_color;
+
+    pros::Color get_color();
+    bool object_detected();
+    pros::Color object_color();
+    pros::Color last_color;
+    double convert_rotation_to_length (double angle);
+    void nearest_hook_to_pose(double pose);
 };
