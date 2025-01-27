6c298fc10effa4edef506095b91baf3a5f795460
trainlover08 <iansmithtrainlover@gmail.com>
2025-01-26 21:59:06 -0600

    added last_color update

--- src/refactor/color_sort.cpp
+++ src/refactor/color_sort.cpp
@@ -6,5 +6,7 @@ Color_Sort::Color_Sort (pros::Optical *optical, pros::Distance *distance) {
 }
 
 Color_Sort::get_color () {
-    return optical->get_rgb().red > optical->get_rgb().blue? pros::Color::red : pros::Color::blue;
+    pros::Color color = optical->get_rgb().red > optical->get_rgb().blue? pros::Color::red : pros::Color::blue;
+    Color_Sort::last_color = color;
+    return color;
 }
