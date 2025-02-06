3a1b800f993436eb192e555e00ac116bc8930b6e
Ian Smith <iansmithtrainlover@gmail.com>
2025-02-06 12:10:22 -0600

    Update constexpr_math.cpp

--- include/MCL/refactor/constexpr_math.cpp
+++ include/MCL/refactor/constexpr_math.cpp
@@ -39,3 +39,19 @@ constexpr double cxprmath::clean_angle_degrees (double t) {
 constexpr double cxprmath::deg_to_rad (double t) {
     return (t / 180.0) * cxprmath::M_PI;
 }
+
+constexpr double cxprmath::abs (double x) {
+    return x > 0.0 ? x : -x;
+}
+
+constexpr double cxprmath::newton_sqrt (double x, double curr, double prev) {
+    return (cxprmath::abs(curr - prev) < cxprmath::EPSILON) ? curr : cxprmath::newton_sqrt(x, 0.5 * (curr + x / curr), curr));
+}
+
+constexpr double cxprmath::sqrt (double x) {
+    return x < 0.0 ? -1.0 : x == 0 ? 0.0 : cxprmath::newton_sqrt(x, x, 0);
+}
+
+constexpr double cxprmath::hypot (double x, double y) {
+    return cxprmath::sqrt(x * x + y * y);
+}
