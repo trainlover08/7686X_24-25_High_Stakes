90a28bdda34a8bcf7e75ee83132cbaed9a80d501
Ian Smith <iansmithtrainlover@gmail.com>
2025-02-09 23:57:33 -0600

    update intake.cpp

--- include/refactor/intake.hpp
+++ include/refactor/intake.hpp
@@ -5,18 +5,35 @@
 #include "pros/colors.hpp"
 #include "pros/motors.hpp"
 #include "color_sort.hpp"
+#include "pros/rtos.hpp"
 #include <vector>
 
 class Intake {
     Intake(pros::Motor* lower_intake, pros::Motor* upper_intake, Color_Sort* lower_sort, Color_Sort* upper_sort);
     Intake(pros::Motor* lower_intake, pros::Motor* upper_intake);
+
+    void move();
+    void move(bool reverse);
+    void move(bool reverse, double percentage);
+    void move_lower(double percentage); 
+    void move_lower(double percentage, bool reverse);
+    void move_upper(double percentage);
+    void move_upper(double percentage, bool reverse);
+    void stop();
+    void stop_lower();
+    void stop_upper();
+    void lb_relief();
+
+    unsigned int lb_relief_timeout = 100;
+
+    /*
     bool is_scheduled_motion();
     std::vector<double> hook_poses;
     void move_hook_to_ready();
     void pickup_and_hold();
     void load_lb();
     void score_ring();
-    void sort_ring();
+    void sort_ring();*/
     
     private:
     pros::Rotation *rotation;
@@ -29,12 +46,12 @@ class Intake {
     Color_Sort* lower_sort;
     Color_Sort* upper_sort;
 
-    pros::Color get_color();
-    bool object_detected();
-    pros::Color object_color();
-    pros::Color last_color;
+    //pros::Color get_color();
+    //bool object_detected();
+    //pros::Color object_color();
+    //pros::Color last_color;
     double convert_angle_to_length (double angle);
-    void nearest_hook_to_pose(double pose);
+    //void nearest_hook_to_pose(double pose);
 
     double diameter = 2.0f;
     double chain_length = 18.0f;
--- src/refactor/intake.cpp
+++ src/refactor/intake.cpp
@@ -26,3 +26,61 @@ double Intake::convert_angle_to_length (double angle) {
      distance = 0.0f > distance ? distance : distance + chain_length;
      return distance;
 }
+
+void Intake::move () {
+     this->lower_intake->move(12000);
+     this->upper_intake->move(12000);
+}
+
+void Intake::move (bool reverse) {
+     if (reverse) {
+          this->lower_intake->move(-12000);
+          this->upper_intake->move(-12000);
+     } else {Intake::move();}
+}
+
+void Intake::move (bool reverse, double percentage) {
+     reverse = 2 * reverse - 1;
+     percentage *= 12000.0 / 100.0;
+     percentage *= reverse;
+     this->lower_intake->move(percentage);
+     this->upper_intake->move(percentage);
+}
+
+void Intake::move_lower (double percentage) {
+     this->lower_intake->move((percentage * 12000.0) / 100.0);
+}
+
+void Intake::move_lower (double percentage, bool reverse) {
+     percentage *= reverse * 2 - 1;
+     this->move_lower(percentage);
+}
+
+void Intake::move_upper (double percentage) {
+     this->upper_intake->move((percentage * 12000.0) / 100.0);
+}
+
+void Intake::move_upper (double percentage, bool reverse) {
+     percentage *= reverse * 2 - 1;
+     this->move_upper(percentage);
+}
+
+void Intake::stop () {
+     this->upper_intake->move(0);
+     this->lower_intake->move(0);
+}
+
+void Intake::stop_lower () {
+     this->lower_intake->move(0);
+}
+
+void Intake::stop_upper () {
+     this->upper_intake->move(0);
+}
+
+// call as a task
+void Intake::lb_relief() {
+     this->move_upper(100, true);
+     pros::delay(this->lb_relief_timeout);
+     this->stop_upper();
+}
