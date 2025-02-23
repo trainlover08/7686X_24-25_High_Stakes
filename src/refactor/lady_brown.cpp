b5063aea9a0b541dd3abdb151ca32e816bf7181e
Ian Smith <iansmithtrainlover@gmail.com>
2025-02-23 11:48:26 -0600

    added correct initalization for motor

--- src/refactor/lady_brown.cpp
+++ src/refactor/lady_brown.cpp
@@ -1,9 +1,12 @@
 #include "refactor/lady_brown.hpp"
+#include "connor.h"
 
 Lady_Brown::Lady_Brown(pros::Motor* motor, pros::adi::Potentiometer* rotation, lemlib::PID* pid) {
     this->motor = motor;
     this->rotation = rotation;
     this->pid = pid;
+    this->motor->set_brake_mode(pros::E_MOTOR_BRAKE_HOLD);
+    this->motor->set_zero_position(((((this->rotation->get_value() - lady_brown_ready_macro)  / 9999.0) * 3.0) * 360.0) + this->motor->get_position()); // look at this is a problem occurs
 }
 
 void Lady_Brown::move_to_position (const Lady_Brown::position& pos) {
