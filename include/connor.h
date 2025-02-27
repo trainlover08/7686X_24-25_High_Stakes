5e74b1e04407d33437b1d611f4c7ca77eb5062bb
Team Laptop <phoenixrising7686x@gmail.com>
2025-02-27 17:22:26 -0600

    more at practice changes

--- include/connor.h
+++ include/connor.h
@@ -3,30 +3,30 @@
 using namespace pros; 
 
 // change the values after #define. should be self explanitory
-#define front_left_drive_motor -3
+#define front_left_drive_motor -13
 #define middle_left_drive_motor -14
-#define back_left_drive_motor -17
-#define front_right_drive_motor 11
-#define middle_right_drive_motor 18
-#define back_right_drive_motor 15
+#define back_left_drive_motor -15
+#define front_right_drive_motor 12
+#define middle_right_drive_motor 17
+#define back_right_drive_motor 16
 #define front_stage_intake_motor 20
-#define hook_stage_intake_motor -5
-#define lady_brown_motor_port 4
+#define hook_stage_intake_motor -8
+#define lady_brown_motor_port 7
 
-#define mogo_mech_port 'G' // make sure digital ports have a set of '' or are of type char
-#define doinker_port 'D'
+#define mogo_mech_port 'D' // make sure digital ports have a set of '' or are of type char
+#define doinker_port 'F'
 #define intake_piston_port 'E'
 
-#define vertical_odom_pod_rotation_sensor 9
-#define lady_brown_pot 'H'
+#define vertical_odom_pod_rotation_sensor 10
+#define lady_brown_pot 'G'
 
-#define imu_port 2
+#define imu_port 11
 
 #define lady_brown_ready_macro 3850
 #define lady_brown_load_macro 15
 #define lady_brown_score_macro 180
 
-#define drivetrain_lateral_p_gain 10.0 //P, I, D. Dont play with these right before match
+#define drivetrain_lateral_p_gain 10.5 //P, I, D. Dont play with these right before match
 #define drivetrain_lateral_i_gain 0.0
 #define drivetrain_lateral_d_gain 70.0
 #define drivetrain_angular_p_gain 2.0
@@ -42,6 +42,9 @@ using namespace pros;
 #define lb_return_i_gain .01
 #define lb_return_d_gain 0.75
 
+#define lady_brown_p 1.4 // this is the value you change to control the firmness of the motor
+#define lady_brown_load_position 30 // this is what you change to change the load position of the lb. its in percents of hte rotation sensor
+
 #define drive_width 12.25 // 1. find the middle of the wheel (not the length, the thickness). 2. measure the the distance from the middle of the wheels on both sides.
 #define wheel_diameter 3.25
 #define max_wheel_rpm 450
@@ -51,15 +54,21 @@ using namespace pros;
 
 #define throttle_curve_intensity 26
 #define steer_curve_intensity 22
-
+ 
 // quick-guide does not support button combos or multiple controllers by default
 #define doinker_button l2
 #define mogo_button b_button
 #define intake_button r1
 #define extake_button l1
+<<<<<<< HEAD
 #define lady_brown_down_button a_button // change macro for now
 #define lady_brown_que_button y_button
 #define lady_brown_up_button r2 // change macro for now
+=======
+#define lady_brown_load_button r2
+#define lady_brown_up_button a_button
+#define lady_brown_down_button b_button
+>>>>>>> 9ece0edd0a8a581c6aad664c09014cb38b73f87e
 
 // generally don't touch things below this line
 #define l2 E_CONTROLLER_DIGITAL_L2
--- src/main.cpp
+++ src/main.cpp
@@ -10,7 +10,7 @@ void initialize() {
     chassis.setPose(0, 0, 0);
     selector.focus();
     chassis.calibrate();
-    lady_brown_rotation.calibrate();
+    lady_brown_rotation.calibrate(); 
 }
 
 void disabled () {
@@ -28,7 +28,7 @@ void autonomous() {
 
 void opcontrol () {
     //chassis.setPose(-55.2, 46.9, 90);
-    //awp_red();
+    awp_red();
     //awp_blue();
     //chassis.setPose(-54.2, -62.0, 90);
     //goal_rush();
@@ -42,11 +42,11 @@ void opcontrol () {
         }
     }*/
     
-    pros::Task intake_task_opcontrol (intake_task);
-    pros::Task drivetrain_task_opcontrol (drive_task);
+    //pros::Task intake_task_opcontrol (intake_task);
+    //pros::Task drivetrain_task_opcontrol (drive_task);
     pros::Task ui_task_opcontrol (ui_task);
-    pros::Task misc_task_opcontrol (misc_task);
-    pros::Task lady_brown_intake_task_opcontrol (lady_brown_intake_task);
+    //pros::Task misc_task_opcontrol (misc_task);
+    //pros::Task lady_brown_intake_task_opcontrol (lady_brown_intake_task);
     
     //pros::Task mcl_task (localize);
 }
